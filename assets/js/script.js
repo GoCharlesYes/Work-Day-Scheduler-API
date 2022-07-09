2c2df56bed3d7bbcb95628e32fb86afb9b0af150
Charles <gocharles2001@gmail.com>
2022-07-09 17:39:16 +0930

    bug fix on script.js so the time updates live instead of frozen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 // Creating function for time on webpage
 function mainTime() {
+    console.log("maintime")
     let clockNow = moment();
-
     // Using jQuery and moment.js to call #currentday id and add text and format it into appropriate format
     $("#currentDay").text(clockNow.format('MMMM Do YYYY, h:mm:ss a'));
-    
-    // setting interval so time updates live every 1000ms/1sec
-    
 }
 
 mainTime();
-setInterval(mainTime(), 1000);
+// setting interval so time updates live every 1000ms/1sec
+setInterval(mainTime, 1000);
